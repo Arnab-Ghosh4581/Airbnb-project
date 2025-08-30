a621229264a05c6b2dfbf2161441e4f488d0ae16
ARNAB GHOSH <arnab4581@gmail.com>
2025-08-30 17:55:51 +0530

    Commited

--- app.js
+++ app.js
@@ -4,8 +4,7 @@ require('dotenv').config();
 // External Module
 const express = require('express');
 const session = require('express-session');
-// const MongoDBStore = require('connect-mongodb-session')(session);
-const MongoStore = require('connect-mongo');
+const MongoDBStore = require('connect-mongodb-session')(session);
 const { default: mongoose } = require('mongoose');
 const multer = require('multer');
 const DB_PATH = process.env.DB_PATH;
@@ -22,26 +21,14 @@ const app = express();
 app.set('view engine', 'ejs');
 app.set('views', 'views');
 
-// const store = new MongoDBStore({
-//   uri: DB_PATH,
-//   collection: 'sessions',
-//   //  useNewUrlParser: true,
-//   // useUnifiedTopology: true
-// })
+const store = new MongoDBStore({
+  uri: DB_PATH,
+  collection: 'sessions',
+   useNewUrlParser: true,
+  useUnifiedTopology: true
+})
 // .then(()=> console.log('DB connected'))
-// .then((error)=> console.log("DB not connected")); 
-const store = MongoStore.create({
-  mongoUrl: DB_PATH,
-  collectionName: 'sessions',
-  ttl: 14 * 24 * 60 * 60 // 14 days
-});
-
-app.use(session({
-  secret: "KnowledgeGate AI with Complete Coding",
-  resave: false,
-  saveUninitialized: true,
-  store
-}));
+// .then((error)=> console.log("DB not connected"));
 
 const randomString = (length) => {
   const characters = 'abcdefghijklmnopqrstuvwxyz';
